6bcf29e91c9f7f65723ada8d5b131f3bb5a6a409
meetroshna <105015407+meetroshna@users.noreply.github.com>
2022-07-25 13:42:00 -0400

    Add files via upload

---
+++ FISH.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+#load libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+import seaborn as sns
+
+
+# In[11]:
+
+
+#load dataset
+df=pd.read_csv(r"C:\Users\roshn\OneDrive\Desktop\Fish.csv")
+
+
+# In[12]:
+
+
+df.head()
+
+
+# In[13]:
+
+
+df.info()
+
+
+# In[14]:
+
+
+df.describe()
+
+
+# In[15]:
+
+
+df.corr()
+
+
+# In[16]:
+
+
+df.isnull().sum()
+
+
+# In[19]:
+
+
+X = df.drop('Species', axis=1)
+y = df['Species']
+
+
+# In[20]:
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=.25, random_state=42)
+
+from sklearn.feature_selection import SelectKBest, f_classif
+
+selector = SelectKBest(f_classif, k=6)
+selector.fit(X_train, y_train)
+
+
+# In[21]:
+
+
+selector.scores_
+
+
+# In[22]:
+
+
+cols = selector.get_support(indices=True)
+cols
+
+
+# In[23]:
+
+
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+encoder = LabelEncoder()
+y = encoder.fit_transform(y)
+y_map = {index:label for index,label in enumerate(encoder.classes_)}
+
+
+# In[24]:
+
+
+y_map
+
+
+# In[25]:
+
+
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+
+# In[26]:
+
+
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+
+# In[27]:
+
+
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+
+# In[30]:
+
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=.25, random_state=42)
+
+
+# In[31]:
+
+
+from sklearn.linear_model import LogisticRegression
+log_model = LogisticRegression()
+log_model.fit(X_train,y_train)
+
+
+# In[32]:
+
+
+log_model.score(X_test,y_test)
+
+
+# In[35]:
+
+
+# Create a Pickle file  
+import pickle
+pickle_out = open("fish.pkl","wb")
+pickle.dump(log_model, pickle_out)
+pickle_out.close()
+
+
+# In[ ]:
+
+
+
+
---
+++ Flask.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+import pickle
+import pandas as pd
+from flask import Flask, request, jsonify, render_template
+
+app=Flask(__name__)
+pickle_in = open("fish.pkl","rb")
+logistic_model = pickle.load(pickle_in)
+
+@app.route('/')
+def home():
+    return render_template('FLASK.html')
+
+
+@app.route('/predict',methods=["POST"])
+def predict():
+    """
+    For rendering results on HTML GUI
+    """
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = logistic_model.predict(final_features)
+    #prediction = logistic_model.predict([[242.0,23.2,25.4,30.0,11.5200,4.0200]])
+    return render_template('FLASK.html', prediction_text = 'THE SPECIES OF THE FISH IS  {}'.format(str(prediction)))
+
+if __name__=='__main__':
+    app.run()
+
+
+# In[ ]:
+
+
+
+
